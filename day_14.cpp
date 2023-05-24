b25a520ee992737e1157d02f7522cd2a5c601224
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2023-05-24 08:00:32 +0100

    day 14 all tests passing, but wrong answer

--- day_14.cpp
+++ day_14.cpp
@@ -7,11 +7,9 @@ int get_factor(const int& required, const int& produced) {
 	if (produced >= required) {
 		return 1;
 	}
-
 	if (required % produced == 0) {
 		return required / produced;
 	}
-
 	int f = required / produced;
 	return f + 1;
 }
@@ -20,37 +18,19 @@ int get_factor(const int& required, const int& produced) {
 void day_14::print_answers() {
 	// read the file into a map
 	std::map<std::string, Reaction> reactions;
-	std::vector<std::string> input_data = helper::read_file("input\\test_14b.txt");
-
+	std::vector<std::string> input_data = helper::read_file("input\\day_14.txt");
 	for (auto& s : input_data) {
 		helper::remove_all_of(s, ' ');
-		std::string product;
-		std::string number;
+		helper::remove_all_of(s, '=');
+		std::string product, number;
 		Input input_list;
-		// build input
-		size_t splitter_pos = s.find("=>");
-		for (size_t i{ 0 }; i < splitter_pos; ++i) {
-			if (s[i] == ',') {
+		for (size_t i{ 0 }; i < s.length(); ++i) {
+			if (std::ispunct(s[i])) {
 				input_list.push_back(std::make_pair(product, std::stoi(number)));
 				number.clear();
 				product.clear();
 			}
 			else if (std::isdigit(s[i])) {
-				number += s[i];
-			}
-			else {
-				product += s[i];
-			}
-		}
-		// get last input product
-		if (!product.empty() && !number.empty()) {
-			input_list.push_back(std::make_pair(product, std::stoi(number))); 
-			number.clear();
-			product.clear();
-		}
-		// get product of input
-		for (size_t i{ splitter_pos + 2 }; i < s.length(); ++i) {
-			if (std::isdigit(s[i])) {
 				number += s[i];
 			}
 			else {
@@ -75,13 +55,28 @@ void day_14::print_answers() {
 	while (!chemical_stack.empty()) {
 		std::pair<std::string, int> next = chemical_stack.top();
 		chemical_stack.pop();
+		if (remainders.find(next.first) != remainders.end()) {
+			int diff = next.second > remainders[next.first] ? remainders[next.first] : next.second;
+			next.second -= diff;
+			remainders[next.first] -= diff;
+		}
 		std::cout << "Require " << next.second << " of " << next.first << '.';
 		start = reactions[next.first];
 		required = get_factor(next.second, start.first);
-		std::cout << "Factor: " << required << '\n';
+		int remainder = next.second % start.first;
+		if (remainder > 0) {
+			if (remainders.find(next.first) != remainders.end()) {
+				remainders[next.first] += start.first - remainder;
+			}
+			else {
+				remainders[next.first] = start.first - remainder;
+			}
+		}
+		std::cout << "Remainder of " << next.first << ": " << remainders[next.first] << '\n';
 		input_chemicals = start.second;
 		for (auto& i : input_chemicals) {
 			if (i.first == "ORE") {
+				remainders[next.first] = 0;
 				if (ore_products.find(next.first) != ore_products.end()) {
 					ore_products[next.first] += next.second;
 				}
@@ -91,16 +86,21 @@ void day_14::print_answers() {
 				break;
 			}
 			i.second *= required;
-			chemical_stack.push(i);
+			if (remainders.find(i.first) != remainders.end()) {
+				int diff = i.second > remainders[i.first] ? remainders[i.first] : i.second;
+				i.second -= diff;
+				remainders[i.first] -= diff;
+			}
+			if (i.second > 0) {
+				chemical_stack.push(i);
+			}
 		}
 	};
 
 	int ore{ 0 };
 	for (const auto& [prod, quantity] : ore_products) {
 		int factor{ get_factor(quantity, reactions[prod].first) };
-		std::cout << prod << ':' << quantity << '\n';
 		ore += reactions[prod].second[0].second * factor;
-		std::cout << reactions[prod].second[0].second * factor << '\n';
 	}
 
 	std::cout << ore;
